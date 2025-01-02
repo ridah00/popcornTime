377c22816c3e7b08ece9e2ce5dfa55074e18faaf
ridha_mahdjoub <mahdjoubridah00@gmail.com>
2025-01-02 12:18:33 +0100

    retouche interface

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ fetch("movies.json")
   .then((movies) => {
     for (item of movies["results"]) {
       document.querySelector("#movie_section").innerHTML += `
-        <div class="card col-lg-2 col-10 ">
-         <img src="https://image.tmdb.org/t/p/w300${item.poster_path}" class="card-img-top" alt="...">
-         <div class="card-body">
-            <p class="card-text">${item.title}</p>
-            <p class="card-text">${item.vote_average}/10</p>
+        <div class="card col-md-2 col-10 ">
+         <img src="https://image.tmdb.org/t/p/w300${item.poster_path}" class="card-img-top" alt="image du film : ${item.title}">
+         <div class="card-body bg-dark ">
+            <p class="card-text text-light">${item.title}</p>
+            <p class="card-text1 text-light">${item.vote_average}/10 <i class="bi bi-star-fill text-warning"></i></p>
          </div>
         </div>
         `;
     }
   });
+  
